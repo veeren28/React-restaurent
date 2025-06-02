5cd78ad66d1b5d74e4251a26a3877334b5a2c6e8
veeren28 <veerenveerupinisetty@gmail.com>
2025-06-02 20:50:18 +0530

    first commit

---
+++ postcss.config.mjs
@@ -0,0 +1,15 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        exo2: ['"Exo 2"', "sans-serif"],
+        outfit: ['"Outfit"', "sans-serif"],
+        roboto: ['"Roboto"', "sans-serif"],
+        tagesschrift: ['"Tagesschrift"', "cursive"],
+        underdog: ['"Underdog"', "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
