8ec307934fcabed739285f40bbf194152874a075
veeren28 <veerenveerupinisetty@gmail.com>
2025-06-02 21:14:27 +0530

    before responive code for landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -10,7 +10,7 @@ const LandingPage = () => {
           {["Home", "About", "Menu", "Contact us"].map((item, i) => (
             <li
               key={i}
-              className="cursor-pointer font-exo2 flex flex-col items-center group px-4 py-2 rounded-md transition-all duration-300 hover:bg-black/20"
+              className="cursor-pointer  flex flex-col items-center group px-4 py-2 rounded-md transition-all duration-300 hover:bg-black/20"
             >
               <span className="transition-colors duration-300 group-hover:text-orange-700">
                 {item}
@@ -22,13 +22,13 @@ const LandingPage = () => {
       </div>
 
       {/* Body */}
-      <div className="flex flex-1 items-center px-20  -mt-40  ">
-        <div>
+      <div className="flex flex-1 items-center px-20    ">
+        <div className="">
           <h2 className="text-white text-6xl">
             Just Order Us and <br />
             <span className="typing"> Start the Day in the Best Way </span>
           </h2>
-          <button className="block p-2 bg-amber-600 rounded-lg w-30 cursor-pointer hover:bg-amber-700 hover:transition-all hover:scale-110 duration-500">
+          <button className=" inline-block p-2  w-25 h-12 bg-amber-600 rounded-lg  cursor-pointer hover:bg-amber-700 hover:transition-all hover:scale-110 duration-500">
             {" "}
             Menu
           </button>
